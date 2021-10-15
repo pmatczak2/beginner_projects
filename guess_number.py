5fcadb7987157ab0a289a291a76d191b11484c51
piotrmatczak <pmatczak2@gmail.com>
2021-10-14 18:56:57 -0700

    Initial commit

---
+++ guess_number.py
@@ -0,0 +1,29 @@
+import random
+
+number_guess = 0
+number = random.randint(1, 20)
+
+my_name = input("Hello, what is your name? ")
+print(f"Welcome to the number guessing game {my_name}. Im thinking of a number between an number between 1 and 20.")
+
+for number_guess in range(6):
+    guess = input("Take a guess: ")
+    guess = int(guess)
+
+    if guess < number:
+        print("you guessed too low")
+
+    if guess > number:
+        print("you guessed too high")
+
+    if guess == number:
+        print("you guessed correctly")
+        break
+
+if guess == number:
+    number_guess = str(number_guess + 1)
+    print(f"Great job {my_name}, you guessed my number in {number_guess} guesses")
+
+if guess != number:
+    number = str(number)
+    print(f"No, that number i was thinking was {number}")
