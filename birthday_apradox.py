a91f030c88d21facf9ef6ef358f55148a4e83356
piotrmatczak <pmatczak2@gmail.com>
2021-10-18 19:30:21 -0700

    generate and display birthdays

--- birthday_apradox.py
+++ birthday_apradox.py
@@ -14,6 +14,34 @@ def get_match(birthdays):
         return None
 
     for a, birthday_a in enumerate(birthdays):
-        for b, birthday_b in enumerate(birthdays[a + 1 :]):
+        for b, birthday_b in enumerate(birthdays[a + 1:]):
             if birthday_a == birthday_b:
                 return birthday_a
+
+print('''The Birthday Paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
+(It's not actually a paradox, it's just a surprising result.)
+''')
+
+months = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct',
+          'Nov', 'Dec')
+
+while True:
+    print("How many birthdays should i generate (max 100)")
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        num_b_days = int(response)
+        break
+print()
+
+print('Here are', num_b_days, 'birthdays:')
+birthdays = get_birthdays(num_b_days)
+for i, birthday in enumerate(birthdays):
+    if i != 0:
+        print(', ', end='')
+    month_name = months[birthday.month - 1]
+    dateText = '{} {}'.format(month_name, birthday.day)
+
