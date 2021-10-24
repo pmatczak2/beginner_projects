57adcf3eaec8897e5985086ccc80d4e5bed45b42
piotrmatczak <pmatczak2@gmail.com>
2021-10-24 08:35:02 -0700

    turtle design

--- generate_pyramid.py
+++ generate_pyramid.py
@@ -1,6 +1,7 @@
 def triangle(n):
     for i in range(0, n):
-       for j in range(0, n):
-        print(i, j)
+        for j in range(0, n):
+            print("0", i)
+        #print("0")
 
-triangle(5)
+triangle(3)
