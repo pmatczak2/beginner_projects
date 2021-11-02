c199ae0c414de9e7697a786d3f057491ec44e248
piotrmatczak <pmatczak2@gmail.com>
2021-11-02 04:45:19 -0700

    oop introduction

--- oop.py
+++ oop.py
@@ -28,9 +28,53 @@ turtle.color('red') # This is the state
 turtle.forward(100) # This is the method
 
 # We can create our very own class with methods!
+# with in this class we can define the methods/operations that our class will do.
+# a method is just a function the goes inside out calls (essentially)
 
 class Dog:
 
-# with in this class we can define the methods/operations that our class will do.
-# a method
+    def color(self):
+        return "brown"
 
+# Now we can create a variable of class, Dog!
+x = Dog()
+print(x)
+# The '__main__' is the module tht hte calss was defined in this is set by degault to main
+
+# We hace created out cory fitst class!
+
+# Now we can explore something known as the initializer
+# This is something EVERY class should hace! It essentially sets the default parameters for our class.
+# So it's similar to how a turtle has its own dafault srttings
+
+# We will set up a new class to demonstrate the initialzier!
+
+class Dogs:
+
+    def __init__(self):
+        self.Labrador = "barnie"
+        self.Poodle = 3
+
+# Define a variable of this class - create instance of the class
+a = Dogs()
+# Shoeing our method in action!
+print(a.Labrador)
+print(a.Poodle)
+# Two instances of the class are not always the same
+b = Dogs()
+print(a == b) # This is false!
+
+# let's look a the extra features you can add
+
+class Animals:
+
+    # initializer
+    def __init__(self, animal_type, animal_age, animal_name):
+        self.type = animal_type
+        self.age = animal_age
+        self.name = animal_name
+
+# Create a new variable
+# Its important to add the specific parameters!
+c = Animals("dog", 10, "bornie")
+d = Animals()
