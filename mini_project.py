a76852b6271ba85a57fc2b8f8d0242e8a66ddf28
piotrmatczak <pmatczak2@gmail.com>
2021-11-03 04:35:12 -0700

    tutorial miniature project

--- mini_project.py
+++ mini_project.py
@@ -7,3 +7,10 @@ print(number_of_exams)
 # Enterin how many credits these exams cover
 total_credits = int(input("Enter how many credits these exams cover: "))
 
+# Begin wiht average of 0 and then add up prcentages form eacg exam
+average = 0
+for exam in range(number_of_exams):
+    score = int(input("Enter exam score: "))
+    exam_credits = int(input("Enter how many credits this exam covered: "))
+    average = average + score*exam_credits/total_credits
+print("Your average is", average)
