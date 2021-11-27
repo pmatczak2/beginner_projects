006f4101d0564df9f4ba80786994fdf8dafbc1ca
piotrmatczak <pmatczak2@gmail.com>
2021-11-27 08:36:31 -0800

    set flags. created a main while loop.

--- black_jack_21.py
+++ black_jack_21.py
@@ -152,10 +152,10 @@ def getHandValue(cards):
         elif rank in ('K', 'Q', 'J'):  #  Face cards are worth 10 points
             value += 10
         else:
-            value += int(rank) #  Numbered cards are worth their number.
+            value += int(rank)  #  Numbered cards are worth their number.
 
     #  Add the values for the aces.
-    value += numberOfAces #  Add 1 per ace
+    value += numberOfAces  #  Add 1 per ace
     for i in range(numberOfAces):
         #  If another 10 can be added with busting, do so:
         if value + 10 <= 21:
